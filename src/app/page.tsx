ad774e36f32bd022566b670080b2ca8d5d286220
AnshV2 <misteransh@gmail.com>
2024-01-26 10:36:54 -0500

    added loading states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,36 @@ import toast, { Toaster } from 'react-hot-toast';
 import Link from 'next/link'
 import { Public } from "@prisma/client/runtime/library";
 import Image from 'next/image'
+import { Loading } from "./_components/loading";
 
 
+const Feed = () => {
+  const {data, isLoading: postsLoading} = api.post.getMany.useQuery()
 
+  if (postsLoading) {
+    return (
+      <div className = "flex h-full w-full justify-center items-center">
+      <Loading />
+      </div>
+    )
+  }
+  return (
+    <>
+      {{data}.data?.map((post) => 
+          {return <div  key = {post.content.id} className = "flex w-full h-28 pl-16 border-t-2 border-b-2  border-slate-500 py-5">
+            <Link href={`/${post.author?.id}`}>
+            <img src = {post.author?.imageUrl} className = "size-16 rounded-full mr-8"></img>
+            </Link>
+            <div className = "">
+              <Link href={`/${post.author?.id}`}>
+                <div key = {post.content.id} className = "text-l relative  text-slate-300">@{post.author?.username} · {dayjs(post.content.createdAt).fromNow()}</div>
+              </Link>
+              <div  className = "text-2xl relative top-2.5">{post.content.content}</div>
+            </div>
+          </div>})}
+    </>
+  )
+}
 
 export default function Home() {
   noStore();
@@ -43,6 +70,8 @@ export default function Home() {
   }
   });
 
+  const {data} = api.post.getMany.useQuery()
+
   return (
     <main className="flex flex-col items-center  h-screen w-screen bg-slate-950">
         <Toaster
@@ -54,24 +83,12 @@ export default function Home() {
           <img src = {user.user?.imageUrl} className = "size-16 rounded-full "></img>
           <input ref = {ref} className = "text-purple-500 m-4 ml-8 bg-transparent outline-none" placeholder="Type some emojis!"></input>
           <button onClick = {() => {
-            if (ref.current != null) {mutate({content: ref.current.value})}
+            if (ref.current != null) {mutate({content: ref.current.value })}
             }} className = "text-slate-200 content-right ml-40">Post</button>
         </div>
 
-        <div className = "flex flex-col w-2/5 h-3/4 overflow-y-scroll border-2 border-slate-500 ">
-          {api.post.getMany.useQuery().data?.map((post) => 
-          {return <div  key = {post.content.id} className = "flex w-full h-28 pl-16 border-t-2 border-b-2  border-slate-500 py-5">
-            <Link href={`/${post.author?.id}`}>
-            <img src = {post.author?.imageUrl} className = "size-16 rounded-full mr-8"></img>
-            </Link>
-            <div className = "">
-              <Link href={`/${post.author?.id}`}>
-                <div key = {post.content.id} className = "text-l relative  text-slate-300">@{post.author?.username} · {dayjs(post.content.createdAt).fromNow()}</div>
-              </Link>
-              <div  className = "text-2xl relative top-2.5">{post.content.content}</div>
-            </div>
-          </div>})}
-
+        <div className = "flex flex-col w-2/5 h-3/4 overflow-y-scroll border-2 border-slate-500">
+            <Feed />
         </div>
 
         <div className = "flex w-2/5 h-35 p-4 pl-16 justify-center content-center  border-2 border-slate-500">
